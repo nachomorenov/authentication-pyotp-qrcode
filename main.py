2385706222c208ac9c10da2cb0296004a2149fd7
Nacho Moreno <nachomorenov04@gmail.com>
2024-12-03 18:48:52 -0300

    terminado

--- main.py
+++ main.py
@@ -1,48 +1,38 @@
 import pyotp
 import qrcode
 from flask import Flask, render_template, request, redirect, url_for, flash
-import io
+from io import BytesIO
 import base64
 
 app = Flask(__name__)
+app.secret_key = "SECRETKEYEXAMPLE"
 
-#Generador TOTP y QR
+#Generador TOTP
 key = pyotp.random_base32()
 totp = pyotp.TOTP(key)
-uri = totp.provisioning_uri(name="John Doe", issuer_name="Example Bank")
 
 #Ruta principal
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
-    message = None
+    uri = totp.provisioning_uri(name="John Doe", issuer_name="Example Bank")
+    qr = qrcode.make(uri)
+    qr_io = BytesIO()
+    qr.save(qr_io, "PNG")
+    qr_io.seek(0)
+    qr_base64 = base64.b64encode(qr_io.getvalue()).decode()
+    return render_template("index.html", qr_code=qr_base64)
 
-    #Generador de QR y convertidor a base64
-    qr_image = qrcode.make(uri)
-    img_io = io.BytesIO()
-    qr_image.save(img_io, "PNG")
-    img_io.seek(0)
-    qr_code_base64 = base64.b64encode(img_io.getvalue()).decode("utf-8")
-
-    if request.method == "POST":
-        #Maneja el envio del formulario
-        if "cancel" in request.form:
-            return redirect(url_for("cancel"))
+@app.route("/verify", methods=["POST"])
+def verify():
+    if "cancel" in request.form:
+        return redirect(url_for("index"))
     
-        user_code = request.form.get("codigo")
-        if totp.verify(user_code):
-            return redirect(url_for("success"))
-        else:
-            message = "Codigo incorrecto. Intentelo de nuevo."
-    
-    return render_template("index.html", qr_code_base64=qr_code_base64, message=message)
-
-@app.route("/success")
-def success():
-    return "<h1>Acceso Concedido.</h1>"
-
-@app.route("/cancel")
-def cancel():
-    return "<h1>Operacion Cancelada.</h1>"
+    code = request.form.get("code")
+    if totp.verify(code):
+        return "Acceso Concedido"
+    else:
+        flash("Codigo Incorrecto. Intente nuevamente.")
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
