61502dbbd9dbb7aedf0a161fc0f8525d6f266403
Nacho Moreno <nachomorenov04@gmail.com>
2024-12-03 19:28:54 -0300

    app final

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
 import pyotp
 import qrcode
-from flask import Flask, render_template, request, redirect, url_for, flash
-from io import BytesIO
+from flask import Flask, render_template, request, redirect, flash
+import io
 import base64
 
 app = Flask(__name__)
 app.secret_key = "SECRETKEYEXAMPLE"
 
-#Generador TOTP
-key = pyotp.random_base32()
-totp = pyotp.TOTP(key)
-
 #Ruta principal
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    uri = totp.provisioning_uri(name="John Doe", issuer_name="Example Bank")
-    qr = qrcode.make(uri)
-    qr_io = BytesIO()
-    qr.save(qr_io, "PNG")
-    qr_io.seek(0)
-    qr_base64 = base64.b64encode(qr_io.getvalue()).decode()
-    return render_template("index.html", qr_code=qr_base64)
-
-@app.route("/verify", methods=["POST"])
-def verify():
-    if "cancel" in request.form:
-        return redirect(url_for("index"))
+    key = pyotp.random_base32()
+    totp = pyotp.TOTP(key)
     
-    code = request.form.get("code")
-    if totp.verify(code):
-        return "Acceso Concedido"
-    else:
-        flash("Codigo Incorrecto. Intente nuevamente.")
-        return redirect(url_for("index"))
+    qr_url = totp.provisioning_uri(name="John Doe", issuer_name="Bank Example")
+    img = qrcode.make(qr_url)
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    
+    qr_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
+    
+    if request.method == "POST":
+        code = request.form["code"]
+        
+        if totp.verify(code):
+            flash("Código correcto! Acceso concedido. Redirigiendo a la pagina principal...", "success")
+        else:
+            flash("Código incorrecto. Inténtalo de nuevo.", "failure")
+        return redirect("/") 
+
+    return render_template("index.html", qr_base64=qr_base64)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
