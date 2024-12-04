a98350b4dd454fd97e714ed3022b138c85f5f33c
Nacho Moreno <nachomorenov04@gmail.com>
2024-12-04 16:53:32 -0300

    FINISH

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
 import pyotp
-import qrcode
-from flask import Flask, render_template, request, redirect, flash
-import io
 import base64
+import qrcode
+from io import BytesIO
 
 app = Flask(__name__)
-app.secret_key = "SECRETKEYEXAMPLE"
+app.secret_key = 'SECRETKEYEXAMPLE'
 
-#Ruta principal
+
+key = pyotp.random_base32()
+totp = pyotp.TOTP(key)
+
+
+def generate_qr_base64():
+    uri = totp.provisioning_uri(name="John Doe", issuer_name="Bank Example")
+    qr = qrcode.QRCode()
+    qr.add_data(uri)
+    qr.make(fit=True)
+    img = qr.make_image(fill="black", back_color="white")
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+    return base64.b64encode(buffer.read()).decode('utf-8')
+
+qr_base64 = generate_qr_base64()
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    key = pyotp.random_base32()
-    totp = pyotp.TOTP(key)
-    
-    qr_url = totp.provisioning_uri(name="John Doe", issuer_name="Bank Example")
-    img = qrcode.make(qr_url)
-    img_io = io.BytesIO()
-    img.save(img_io, 'PNG')
-    img_io.seek(0)
-    
-    qr_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
-    
     if request.method == "POST":
-        code = request.form["code"]
-        
-        if totp.verify(code):
-            flash("Código correcto! Acceso concedido. Redirigiendo a la pagina principal...", "success")
+        user_code = request.form.get("code")
+        if totp.verify(user_code):
+            flash("Código verificado con éxito.", "success")
         else:
-            flash("Código incorrecto. Inténtalo de nuevo.", "failure")
-        return redirect("/") 
+            flash("Código incorrecto. Intenta nuevamente.", "danger")
+        return redirect(url_for("index"))
 
     return render_template("index.html", qr_base64=qr_base64)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
